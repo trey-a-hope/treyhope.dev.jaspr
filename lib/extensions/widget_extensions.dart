13a43f6fa63e916d5d16149b7c6d5a94b030f8c0
Trey Hope <trey.a.hope@gmail.com>
2025-12-24 10:26:35 -0500

    feat: Add 'under construction' Lottie animation, update loading animation, and refactor toastification integration.

--- lib/extensions/widget_extensions.dart
+++ lib/extensions/widget_extensions.dart
@@ -1,26 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:toastification/toastification.dart';
 
 extension WidgetExtensions on Widget {
-  Widget materialApp() => ToastificationWrapper(
-    child: MaterialApp(
-      darkTheme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.dark,
-        textTheme: GoogleFonts.robotoTextTheme(),
-      ),
-      theme: ThemeData(
-        useMaterial3: true,
-        brightness: Brightness.light,
-        textTheme: GoogleFonts.robotoTextTheme(),
-      ),
-      themeMode: ThemeMode.light,
-      debugShowCheckedModeBanner: false,
-      home: Material(
-        color: Colors.transparent,
-        child: this,
-      ),
+  Widget materialApp() => MaterialApp(
+    darkTheme: ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.dark,
+      textTheme: GoogleFonts.robotoTextTheme(),
+    ),
+    theme: ThemeData(
+      useMaterial3: true,
+      brightness: Brightness.light,
+      textTheme: GoogleFonts.robotoTextTheme(),
+    ),
+    themeMode: ThemeMode.light,
+    debugShowCheckedModeBanner: false,
+    home: Material(
+      color: Colors.transparent,
+      child: this,
     ),
   );
 }
